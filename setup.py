085e61abf3fe547da04ce7bb94fc87df626783de
Sergey Anufriev <35104456+SergeyAnufriev@users.noreply.github.com>
2019-09-12 15:05:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='optimization',
+setup(name='optimization_1',
       version='0.0.1',
       install_requires=['gym'])#And any other dependencies required)
 
